11441097e19351f23d48059ca795eca216023038
Jary Chen <jarychenu24@gmail.com>
2025-05-27 17:27:36 -0400

    Basic Home.jsx
    Added models and assets for the 3d rendering
    
    Signed-off-by: Jary Chen <jarychenu24@gmail.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
+import { Island } from "../models/island";
 
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -8,13 +9,33 @@ import Loader from "../components/Loader";
       </div> */
 }
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+      screenPosition = [0, -6.5, -43];
+    }
+    else{
+      screenScale = [1, 1, 1];
+      screenPosition = [0, -6.5, -43];
+    }
+  }
   return (
     <section className="w-full h-screen relative">
       <Canvas
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
       >
-        <Suspense fallback={<Loader />}></Suspense>
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+
+          <Island />
+        </Suspense>
       </Canvas>
     </section>
   );
