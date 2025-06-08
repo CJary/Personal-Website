b658873aa7def1c08f2d9ec7f7b60a19b5ffd872
Jary Chen <jarychenu24@gmail.com>
2025-06-08 12:10:23 -0400

    Alert system
    Custom hook to reuse Alert. Custom usefunction to change state of alert
    
    Signed-off-by: Jary Chen <jarychenu24@gmail.com>

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,8 @@ import emailjs from "@emailjs/browser";
 import { Canvas } from "@react-three/fiber";
 import Fox from "../models/Fox";
 import Loader from "../components/Loader";
+import useAlert from "../hooks/useAlert";
+import Alert from "../components/Alert";
 
 const Contact = () => {
   const formRef = useRef(null);
@@ -13,6 +15,8 @@ const Contact = () => {
   });
   const [isLoading, setIsLoading] = useState(false);
   const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+  const { alert, showAlert, hideAlert } = useAlert();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -37,10 +41,14 @@ const Contact = () => {
       )
       .then(() => {
         setIsLoading(false);
-        // TODO show success message
-        // TODO hide an alert
+        showAlert({
+          show: true,
+          text: "Thank you. I will get back to you as soon as possible.",
+          type: "success",
+        });
 
         setTimeout(() => {
+          hideAlert();
           setCurrentAnimation("idle");
           setForm({
             name: "",
@@ -53,7 +61,11 @@ const Contact = () => {
         setIsLoading(false);
         setCurrentAnimation("idle");
         console.log(error);
-        // TODO show error message
+        showAlert({
+          show: true,
+          text: "Something went wrong. Please try again later.",
+          type: "danger",
+        });
       });
   };
   const handleFocus = () => setCurrentAnimation("walk");
@@ -61,6 +73,8 @@ const Contact = () => {
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert} />}
+
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
         <form
